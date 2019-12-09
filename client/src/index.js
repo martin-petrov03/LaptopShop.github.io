f9c56ef22c43606c411c887531786ff189ed5c24
martinpetrov000 <martin.petrov033@gmail.com>
2019-12-09 17:40:34 +0200

    Client Side

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App from './App';
+// import AuthContextProvider from './contexts/AuthContext';
+
+ReactDOM.render(
+    // <AuthContextProvider>
+        <Router>
+            <App />
+        </Router>
+    // </AuthContextProvider>
+    , document.getElementById('root'));
+serviceWorker.unregister();

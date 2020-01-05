53bf17a4407b69d248619b0a2fc80dd787df3072
martinpetrov000 <martin.petrov033@gmail.com>
2020-01-05 21:28:18 +0200

    Add laptops filter

--- client/src/components/Laptops/Laptops.jsx
+++ client/src/components/Laptops/Laptops.jsx
@@ -1,29 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 import Laptop from './Laptop';
 import './index.css';
 
 const Laptops = (props) => {
     const [laptops, setLaptops] = useState([]);
+    const [filteredData, setData] = useState([]);
 
     useEffect(() => {
         const fetchData = () => {
             axios.get('http://localhost:3001/laptops/all')
-                .then(res => {                      
+                .then(res => {
                     if(res.status === 200) {
                         setLaptops(res.data.laptops);
+                        setData([])
                     }
-                })                
+                })
         }
         fetchData();
     }, []);
 
+    const handleModelChange = (event) => {
+        setData([]);
+        console.log(filteredData)
+        laptops.filter(l =>  {            
+            if(l.model.toLowerCase().includes(event.target.value)) {
+                const index = filteredData.findIndex(x => x.model==l.model)
+                if (index === -1){                    
+                    setData(filteredData.concat(l));
+                }
+            }
+        });
+    }
+
     return (
-        <main className="laptops-container">            
-            {
-                <Laptop data={laptops} />
-            }       
-        </main>
+        <Fragment>
+            <div className="filter">
+                <label htmlFor="model">Model: </label>
+                <input type="text" id="model" placeholder="Model" onChange={handleModelChange} />
+                <label htmlFor="min-price">Price: </label>
+                <input type="number" id="min-price" placeholder="Min" />
+                <input type="number" id="max-price" placeholder="Max" />
+            </div>
+            <main className="laptops-container">            
+                {
+                    filteredData.length > 0 ? <Laptop data={filteredData} /> : <Laptop data={laptops} />
+                    
+                }                
+            </main>
+        </Fragment>
     );    
 }
 

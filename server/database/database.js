7913aea934469994905de6f0be56bf3d0ac3957c
martinpetrov000 <martin.petrov033@gmail.com>
2020-07-20 21:16:08 +0300

    Migrate to mongo atlas

--- server/database/database.js
+++ server/database/database.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+const uri = 'mongodb+srv://Admin:admin@cluster0.7amxv.mongodb.net/LaptopShop?retryWrites=true&w=majority';
 
 mongoose.Promise = global.Promise;
 module.exports = () => {
     mongoose.set('useCreateIndex', true);
-    mongoose.connect('mongodb://localhost:27017/laptop-shop', {
-        useNewUrlParser: true        
-    });       
+    //mongodb+srv://Admin:<password>@cluster0-ehp1t.mongodb.net/<dbname>?retryWrites=true&w=majority
+    //mongodb://localhost:27017/laptop-shop
+  
+    mongoose.connect(uri, {
+        useNewUrlParser: true
+    });
+    
     const db = mongoose.connection;
     db.once('open', err => {
         if (err) {
